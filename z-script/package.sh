725efc9dc2d2e1dbe2c556a1e04c541345cdbcbf
xiaqiulei <xiaqiulei@126.com>
2023-02-28 15:58:16 +0800

    update

--- z-script/package.sh
+++ z-script/package.sh
@@ -13,3 +13,20 @@ APP_NAME=bssh
 pyinstaller main.py --name "$APP_NAME" --noconsole --windowed \
       --osx-bundle-identifier cn.mycommons.bssh \
       --hidden-import PySide2.QtXml
+
+d="Darwin"
+l="Linux"
+os=$(uname)
+
+m=$(uname -m)
+zipName="bssh-$os-$m.zip"
+zipName="$(tr "[:upper:]" "[:lower:]" <<< "$zipName")"
+echo "zipName = $zipName"
+
+if [[ $os =~ $d ]];then
+    cd dist && zip "$zipName" bssh.app -r
+elif [[ $os =~ $l ]];then
+    cd dist && zip "$zipName" bssh -r
+else
+    echo "can not zip on $os"
+fi
